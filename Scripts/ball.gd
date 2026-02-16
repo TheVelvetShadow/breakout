47b1ba27e1f3a5c792e138e3afb2ac38c0cf3d20
TheVelvetShadow <matt@thevelvetshadow.com>
2026-02-16 20:31:45 +0000

    added ball & walls

---
+++ Scripts/ball.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+var speed = 200.0
+var direction = Vector2.DOWN
+var is_active = true
+
+func _ready() -> void:
+	velocity = Vector2(speed * -1, speed)
+
+func _physics_process(delta: float) -> void:	
+	
+	if is_active:
+		var collision = move_and_collide(velocity * delta)
+		
+		if collision:
+			velocity = velocity.bounce(collision.get_normal())
+		
+		if(velocity.y > 0 and velocity.y <100):
+			velocity.y = -200
+		 
+		if velocity.x == 0:
+			velocity.x = -200		
